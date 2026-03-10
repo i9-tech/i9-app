3b20f3fb40bc85d7a772fa5790c5fd42e98bc9bc
ysilvv <yasmim.silva@sptech.school>
2026-03-09 22:48:03 -0300

    fix: aplicacao de conteudo da aula de app - Implementação de rotas usando pastas e ajuste em layout de telas internas

---
+++ app/ajuda/index.jsx
@@ -0,0 +1,64 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Dashboard() {
+  return (
+    <View style={styles.safe}>
+      
+      <View style={styles.container}>
+        
+        <Ionicons
+          name="construct-outline"
+          size={70}
+          color="#1E22AA"
+        />
+
+        <Text style={styles.titulo}>
+          Tela de Ajuda em construção
+        </Text>
+
+        <Text style={styles.subtitulo}>
+          Esta tela ainda está sendo desenvolvida.
+          {"\n"}
+          Por favor aguarde a próxima atualização do aplicativo.
+        </Text>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  container: {
+    alignItems: "center",
+    paddingHorizontal: 30,
+  },
+
+  titulo: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#333",
+    textAlign: "center",
+  },
+
+  subtitulo: {
+    marginTop: 10,
+    fontSize: 14,
+    color: "#666",
+    textAlign: "center",
+    lineHeight: 20,
+  },
+});
