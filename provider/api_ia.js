e33da1fa2bab00ad00b07dbc607062493969f849
m4yk0n <maykon.rodrigues@sptech.school>
2026-04-15 21:01:08 -0300

    fix: url da ia

--- provider/api_ia.js
+++ provider/api_ia.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {enviroments} from "../utils/enviroments"
 
 const api_ia = axios.create({
-  baseURL: "http://10.18.33.18:8000/",
+  baseURL: "http://0.0.0.0:8000/",
   headers: {
     "Content-Type": "application/json",
   },
